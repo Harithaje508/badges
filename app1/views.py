3ef6d5a42dcc8312695d2260dd49f4388e5c8941
Harithaje508 <haritha.errolla@gmail.com>
2023-11-29 12:19:59 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def b1(request):
+    return render(request,'b1.html')
+def b2(request):
+    return render(request,'b2.html')
+def b3(request):
+    return render(request,'b3.html')
+def b4(request):
+    return render(request,'b4.html')
